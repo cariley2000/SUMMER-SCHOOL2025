7676b638ce076c5c91c844344fec43ff8a7f13dd
cariley2000 <christopher.riley@nottingham.ac.u>
2024-07-10 11:55:25 +0100

    First Commit

---
+++ contest/__init__.py
@@ -0,0 +1,55 @@
+from otree.api import *
+
+
+doc = """
+Your app description
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'contest'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    pass
+
+
+# PAGES
+class Intro(Page):
+    pass
+
+
+class SetupRound(WaitPage):
+    pass
+
+
+class Decision(Page):
+    pass
+
+class WaitForDecision(Page):
+    pass
+
+class Results(Page):
+    pass
+
+class EndBlock(Page):
+    pass
+
+page_sequence = [
+    Intro,
+    SetupRound,
+    Decision,
+    WaitForDecision,
+    Results,
+    EndBlock,
+]
